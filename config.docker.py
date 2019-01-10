7a8d83d588012ccc2f94fa8815e94dc724fc2e03
Michał Leszczyński <michal.leszczynski@cert.pl>
2019-01-10 18:20:11 +0100

    make INDEXABLE_PATHS empty by default (except for running from docker-compose)

--- config.docker.py
+++ config.docker.py
@@ -6,7 +6,7 @@ BACKEND = 'tcp://ursadb:9281'
 REDIS_HOST = 'redis'
 REDIS_PORT = 6379
 SECRET_KEY = os.environ['SECRET_KEY']
-INDEXABLE_PATHS = ['/mnt/samples']
+INDEXABLE_PATHS = [p for p in os.environ.get('INDEXABLE_PATHS', '').split(';') if p.strip()]
 INDEX_TYPE = ['gram3', 'hash4', 'text4', 'wide8']
 METADATA_EXTRACTORS = [
     CuckooAnalysisMetadata("/mnt/samples/analyses/")
