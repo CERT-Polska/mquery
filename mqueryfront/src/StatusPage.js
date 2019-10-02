ad547157cf53990851b8d5ce42330a0ad58c6e6d
Michał Leszczyński <michal.leszczynski@cert.pl>
2019-10-02 10:55:28 +0200

    Update StatusPage.js

--- mqueryfront/src/StatusPage.js
+++ mqueryfront/src/StatusPage.js
@@ -9,7 +9,7 @@ class StatusPage extends Component {
                 <h1 className="text-center mq-bottom">Status</h1>
                 <div className="row">
                     <div className="col-md-6">
-                        <h2 className="text-center mq-bottom">connections</h2>
+                        <h2 className="text-center mq-bottom">current connections</h2>
                         <BackendStatus />
                     </div>
                     <div className="col-md-6">
