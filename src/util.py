232c0162e4e085f8195067a2436dde1c6e145248
dsk <damian.skiedrzynski@nask.pl>
2020-02-13 15:43:35 +0100

    Changing util.py for running tests.

--- src/util.py
+++ src/util.py
@@ -10,10 +10,10 @@ LOG_FORMAT = "[%(asctime)s][%(levelname)s] %(message)s"
 LOG_DATEFMT = "%d/%m/%Y %H:%M:%S"
 
 
-def setup_logging():
+def setup_logging() -> None:
     logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=LOG_DATEFMT)
 
 
-def make_redis():
+def make_redis() -> StrictRedis:
     return StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT, decode_responses=True)
 
