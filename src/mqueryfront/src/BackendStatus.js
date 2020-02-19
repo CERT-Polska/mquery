37ba931281c486d6b122b96d2f702d05169e30f1
Krzysztof Wielocha <krzysztof.wielocha@nask.pl>
2020-02-19 13:09:34 +0100

    poprawiony status wersji jednak nie działa funcja do pobierania informacji o wersji ursadb -> do sprawdzenia

--- src/mqueryfront/src/BackendStatus.js
+++ src/mqueryfront/src/BackendStatus.js
@@ -1,7 +1,4 @@
 import React, {Component} from 'react';
-import ErrorBoundary from './ErrorBoundary';
-import axios from 'axios';
-import {API_URL} from "./config";
 
 
 class BackendJobRow extends Component {
@@ -20,48 +17,30 @@ class BackendJobRow extends Component {
 class BackendStatus extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            jobs: [],
-            error: null
-        }
-    }
-
-    componentDidMount() {
-        axios
-            .get(API_URL + "/backend")
-            .then(response => {
-                this.setState({"jobs": response.data.tasks});
-            })
-            .catch(error => {
-                this.setState({"error": error});
-            });
     }
 
     render() {
-        const backendJobRows = this.state.jobs
+        const backendJobRows = this.props.jobs
             .map((job) =>
                 <BackendJobRow {...job} key={job.id}/>
             );
 
         return (
-            <ErrorBoundary error={this.state.error}>
-                <div className="table-responsive">
-                    <table className="table table-striped table-bordered">
-                        <thead>
-                        <tr>
-                            <th>id</th>
-                            <th>conn</th>
-                            <th>request</th>
-                            <th>progress</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            {backendJobRows}
-                        </tbody>
-                    </table>
-                </div>
-            </ErrorBoundary>
+            <div className="table-responsive">
+                <table className="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>conn</th>
+                        <th>request</th>
+                        <th>progress</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {backendJobRows}
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
