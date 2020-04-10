a3bb4d3db3fd71566231f7260625ac58014e6749
msm-code <msm@tailcall.net>
2020-04-10 19:49:23 +0200

    Disable job GC by default (#80)
    Right now the default config is to remove old result searches
    after as low as 60 hours. This is probably unexpected, and in most
    cases unnecessary. This commit disables GC by default

--- src/config.docker.py
+++ src/config.docker.py
@@ -8,7 +8,7 @@ BACKEND = os.environ.get("MQUERY_BACKEND", "tcp://ursadb:9281")
 REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
 JOB_EXPIRATION_MINUTES = int(
-    os.environ.get("JOB_EXPIRATION_MINUTES", 3600)
-)  # 60 hours
+    os.environ.get("JOB_EXPIRATION_MINUTES", 0)
+)  # infinite by default
 
 METADATA_EXTRACTORS: List[Metadata] = []
