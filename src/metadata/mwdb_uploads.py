2ba1c4d5af4a68af177aee07129e36290f80a2b5
psrok1 <pawel.srokosz@cert.pl>
2020-04-10 17:52:27 +0200

    Reformatted by black

--- src/metadata/mwdb_uploads.py
+++ src/metadata/mwdb_uploads.py
@@ -12,9 +12,12 @@ class MalwarecageUploadsMetadata(Metadata):
     :param mwdb_api_url: API URL accessible from mquery daemon
     :param mwdb_url: Malwarecage URL accessible for mquery users
     """
+
     __depends_on__: List[Any] = []
 
-    def __init__(self, mwdb_api_token: str, mwdb_api_url: str, mwdb_url: str) -> None:
+    def __init__(
+        self, mwdb_api_token: str, mwdb_api_url: str, mwdb_url: str
+    ) -> None:
         super().__init__()
         self.mwdb = Malwarecage(api_url=mwdb_api_url, api_key=mwdb_api_token)
         self.mwdb_url = mwdb_url
@@ -22,7 +25,9 @@ class MalwarecageUploadsMetadata(Metadata):
     def extract(self, matched_fname, current_meta):
         # '/uploads' Malwarecage directory format
         # /mnt/samples/9/d/c/5/9dc571ae13a62954155999cae9cecc4f0689e2ba9a8940f81d1e564271507a3e
-        m = re.search(r"/[a-f0-9]/[a-f0-9]/[a-f0-9]/[a-f0-9]/([a-f0-9]+)$", matched_fname)
+        m = re.search(
+            r"/[a-f0-9]/[a-f0-9]/[a-f0-9]/[a-f0-9]/([a-f0-9]+)$", matched_fname
+        )
 
         if not m:
             return {}
@@ -38,19 +43,17 @@ class MalwarecageUploadsMetadata(Metadata):
 
         if sample:
             for tag in sample.tags:
-                query = urllib.parse.urlencode({
-                    "q": f'tag:"{tag}"'
-                })
+                query = urllib.parse.urlencode({"q": f'tag:"{tag}"'})
                 # Add queryable metadata for each tag from Malwarecage
                 metadata[f"mwdb_tag_{tag}"] = {
                     "display_text": tag,
-                    "url": f"{self.mwdb_url}/?{query}"
+                    "url": f"{self.mwdb_url}/?{query}",
                 }
 
             # Add metadata with link to sample in Malwarecage instance
             metadata[f"mwdb_analysis"] = {
                 "display_text": "mwdb",
-                "url": f"{self.mwdb_url}/sample/{binary_hash}"
+                "url": f"{self.mwdb_url}/sample/{binary_hash}",
             }
 
             job_id = current_meta["job"]
