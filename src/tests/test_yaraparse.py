5511261d4304cd90910f2818124a1f38c77fa000
dsk <dsk@dsk.pl>
2020-04-27 10:56:44 +0200

    Finished implementation of UrsaExpression literal by writing two functions - literal and literal_to_hex. Unit tests for both functions added.

--- src/tests/test_yaraparse.py
+++ src/tests/test_yaraparse.py
@@ -4,24 +4,23 @@ Unit tests for yaraparse
 
 from lib.yaraparse import ursify_hex
 from lib.yaraparse import ursify_plain_string
-from yaramod import PlainString
-from yaramod import YaraRuleBuilder
 import yaramod
 
 
-def test_literal_with_hex():
+def test_literal():
     hex_str = "3F2504E0"
     result = ursify_hex(hex_str)
 
     assert result.query == "{3F2504E0}"
 
 
-def test_literal_without_hex():
-    rule = yaramod.YaraRuleBuilder().with_plain_string("$str", "abc")
-    print(rule.plain_string)
+def test_literal_to_hex():
+    rule = yaramod.YaraRuleBuilder().with_plain_string("$str", "abc").get()
 
-    ascii_str = PlainString("$str", "abc")
+    new_file = yaramod.YaraFileBuilder()
+    yara_file = new_file.with_rule(rule).get()
 
+    ascii_str = yara_file.rules[0].strings[0]
     result = ursify_plain_string(ascii_str)
 
-    print(f"Result: {result.query}")
+    assert result.query == "{616263}"
