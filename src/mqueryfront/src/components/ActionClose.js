d13e7e90daa70e8e1cd975b7dfcf7d02283b5ef7
JaroPowerH <63407380+JaroPowerH@users.noreply.github.com>
2020-04-30 22:47:21 +0200

    feature/#21#24, fontawesome and prop-types installation (#129)
    * feature/#21#24, fontawesome and prop-types installation
    
    * feature/#21#24, package.json corrected
    
    * Update src/mqueryfront/src/RecentPage.js
    
    Co-authored-by: msm-code <msm2e4d534d@gmail.com>
    
    * Update src/mqueryfront/src/SearchJobs.js
    
    Co-authored-by: msm-code <msm2e4d534d@gmail.com>
    
    * Update src/mqueryfront/src/SearchJobs.js
    
    Co-authored-by: msm-code <msm2e4d534d@gmail.com>
    
    * Update src/mqueryfront/src/SearchJobs.js
    
    Co-authored-by: msm-code <msm2e4d534d@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: msm-code <msm@tailcall.net>
    Co-authored-by: msm-code <msm2e4d534d@gmail.com>

---
+++ src/mqueryfront/src/components/ActionClose.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import PropTypes from "prop-types";
+
+const ActionClose = (props) => {
+    return (
+        <button className="btn shadow-none" onClick={props.onClick}>
+            <span data-toggle="tooltip" title="close">
+                <FontAwesomeIcon icon={faTimes} size={props.size} />
+            </span>
+        </button>
+    );
+};
+
+ActionClose.defaultProps = {
+    size: "1x",
+};
+
+ActionClose.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    size: PropTypes.oneOf([
+        "lg",
+        "xs",
+        "sm",
+        "1x",
+        "2x",
+        "3x",
+        "4x",
+        "5x",
+        "6x",
+        "7x",
+        "8x",
+        "9x",
+        "10x",
+    ]),
+};
+
+export default ActionClose;
