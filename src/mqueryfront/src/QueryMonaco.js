5305439cd1e36d8118ad6349ca75ffa9e61bebcf
JaroPowerH <jaroslaw.moch@gmail.com>
2020-05-19 20:33:36 +0200

    feature/#158 QueryMonaco: editor.setValue() code removed

--- src/mqueryfront/src/QueryMonaco.js
+++ src/mqueryfront/src/QueryMonaco.js
@@ -74,9 +74,6 @@ class QueryMonaco extends Component {
         if (this.props.error !== prevProps.error) {
             this.setError(...this.props.error);
         }
-        if (prevProps.rawYara !== "" && this.props.rawYara === "") {
-            this.editor.setValue("");
-        }
     }
 
     render() {
