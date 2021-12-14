269fc6ac065e7e817706bec449925c46f328bc37
msm <msm@tailcall.net>
2021-12-14 01:22:18 +0100

    Simplify the code a bit

--- src/mqueryfront/src/App.js
+++ src/mqueryfront/src/App.js
@@ -5,41 +5,9 @@ import QueryPage from "./query/QueryPage";
 import RecentPage from "./recent/RecentPage";
 import StatusPage from "./status/StatusPage";
 import ConfigPage from "./config/ConfigPage";
-import LoadingPage from "./components/LoadingPage";
 import axios from "axios";
 import { API_URL } from "./config";
 import "./App.css";
-
-function getRoutes(config) {
-    if (!config) {
-        return <LoadingPage />;
-    }
-
-    return (
-        <Routes>
-            <Route exact path="/" element={<QueryPage config={config} />} />
-            <Route
-                path="/query/:hash"
-                element={<QueryPage config={config} />}
-            />
-            <Route
-                exact
-                path="/recent"
-                element={<RecentPage config={config} />}
-            />
-            <Route
-                exact
-                path="/config"
-                element={<ConfigPage config={config} />}
-            />
-            <Route
-                exact
-                path="/status"
-                element={<StatusPage config={config} />}
-            />
-        </Routes>
-    );
-}
 
 function App() {
     const [config, setConfig] = useState(null);
@@ -53,7 +21,28 @@ function App() {
     return (
         <div className="App">
             <Navigation />
-            {getRoutes(config)}
+            <Routes>
+                <Route exact path="/" element={<QueryPage config={config} />} />
+                <Route
+                    path="/query/:hash"
+                    element={<QueryPage config={config} />}
+                />
+                <Route
+                    exact
+                    path="/recent"
+                    element={<RecentPage config={config} />}
+                />
+                <Route
+                    exact
+                    path="/config"
+                    element={<ConfigPage config={config} />}
+                />
+                <Route
+                    exact
+                    path="/status"
+                    element={<StatusPage config={config} />}
+                />
+            </Routes>
         </div>
     );
 }
