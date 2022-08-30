17d1555975ea6572ea3fd550e7cc443aedc5131a
msm <msm@tailcall.net>
2022-08-30 02:42:20 +0200

    Hide elements inaccessible to the current user

--- src/mqueryfront/src/Navigation.js
+++ src/mqueryfront/src/Navigation.js
@@ -6,9 +6,13 @@ import { isAuthEnabled } from "./utils";
 function Navigation(props) {
     let loginElm = null;
     let authEnabled = isAuthEnabled(props.config);
+    let isAdmin = false;
     if (!authEnabled) {
         loginElm = null;
     } else if (props.session != null) {
+        const clientId = props.config["openid_client_id"];
+        const userRoles = props.session["resource_access"][clientId]["roles"];
+        isAdmin = userRoles.includes("admin");
         loginElm = (
             <li className="nav-item nav-right">
                 <a className="nav-link" href="/" onClick={props.logout}>
@@ -58,16 +62,20 @@ function Navigation(props) {
                             Recent jobs
                         </Link>
                     </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to={"/config"}>
-                            Config
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to={"/status"}>
-                            Status
-                        </Link>
-                    </li>
+                    {isAdmin ? (
+                        <li className="nav-item">
+                            <Link className="nav-link" to={"/config"}>
+                                Config
+                            </Link>
+                        </li>
+                    ) : null}
+                    {isAdmin ? (
+                        <li className="nav-item">
+                            <Link className="nav-link" to={"/status"}>
+                                Status
+                            </Link>
+                        </li>
+                    ) : null}
                 </ul>
                 <ul className="navbar-nav navbar-right">
                     <li className="nav-item nav-right">
