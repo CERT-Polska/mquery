6ba9d00cef728dfacdbdb9425ad35a798c6b5731
msm-code <msm@tailcall.net>
2023-01-11 18:10:16 +0100

    Make login faster - no unnecessary redirects (#322)
    * Make login faster - no unnecessary redirects
    
    * reformat

--- src/mqueryfront/src/utils.js
+++ src/mqueryfront/src/utils.js
@@ -18,3 +18,14 @@ export const getProgressBarClass = (status) => {
 
 export const isAuthEnabled = (config) =>
     config && config["auth_enabled"] && config["auth_enabled"] !== "false";
+
+export const openidLoginUrl = (config) => {
+    const login_url = new URL(config["openid_url"] + "/auth");
+    login_url.searchParams.append("client_id", config["openid_client_id"]);
+    login_url.searchParams.append("response_type", "code");
+    login_url.searchParams.append(
+        "redirect_uri",
+        window.location.origin + "/auth"
+    );
+    return login_url;
+};
