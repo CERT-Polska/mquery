a849815f9fb21837e662f64c9c096df23ee9f608
msm-code <msm@tailcall.net>
2023-01-20 17:31:43 +0100

    Check exceptions during filtering using plugins (#338)

--- src/tasks.py
+++ src/tasks.py
@@ -90,11 +90,11 @@ class Agent:
         num_files = len(files)
         self.db.job_start_work(job.id, num_files)
 
-        filemap_raw = {name: self.plugins.filter(name) for name in files}
-        filemap = {k: v for k, v in filemap_raw.items() if v}
-
-        for orig_name, path in filemap.items():
+        for orig_name in files:
             try:
+                path = self.plugins.filter(orig_name)
+                if not path:
+                    continue
                 matches = rule.match(path)
                 if matches:
                     self.update_metadata(
@@ -106,6 +106,9 @@ class Agent:
                 num_errors += 1
             except FileNotFoundError:
                 logging.error("Failed to open file %s", orig_name)
+                num_errors += 1
+            except Exception:
+                logging.exception("Unknown error (plugin?): %s", orig_name)
                 num_errors += 1
 
         self.plugins.cleanup()
