99c41239dcc1b591a58ac434eb24c7f6cc9b5436
msm-code <msm@tailcall.net>
2023-01-27 14:57:08 +0100

    Fix the hamburger menu in bootstrap 5 (#346)

--- src/mqueryfront/src/Navigation.js
+++ src/mqueryfront/src/Navigation.js
@@ -46,8 +46,8 @@ function Navigation(props) {
                 <button
                     className="navbar-toggler"
                     type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent"
                     aria-expanded="false"
                     aria-label="Toggle navigation"
