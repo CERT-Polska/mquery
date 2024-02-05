dba596aa3481d82d13e6aa85fc886e320a7541a0
msm <msm@cert.pl>
2024-02-05 15:19:55 +0100

    Add a proper view model for Job

--- src/app.py
+++ src/app.py
@@ -27,7 +27,7 @@ from .db import Database
 from .lib.yaraparse import parse_yara
 from .plugins import PluginManager
 from .lib.ursadb import UrsaDb
-from .models.job import Job
+from .models.job import Job, JobView
 from .schema import (
     JobsSchema,
     RequestConfigEdit,
@@ -492,7 +492,7 @@ def matches(
 
 @app.get(
     "/api/job/{job_id}",
-    response_model=Job,
+    response_model=JobView,
     tags=["stable"],
     dependencies=[Depends(can_view_queries)],
 )
