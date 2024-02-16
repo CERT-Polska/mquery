66af9dcdea7ab6fedf92db771025280ec81349f6
msm-cert <156842376+msm-cert@users.noreply.github.com>
2024-02-16 12:50:24 +0000

    Migrate to alembic (#382)
    Migrate to alembic

---
+++ src/migrations/env.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from alembic import context
+from sqlmodel import SQLModel
+from logging.config import fileConfig
+
+from mquery.config import app_config  # type: ignore
+from mquery.models.agentgroup import AgentGroup  # type: ignore # noqa
+from mquery.models.configentry import ConfigEntry  # type: ignore # noqa
+from mquery.models.job import Job  # type: ignore # noqa
+from mquery.models.match import Match  # type: ignore # noqa
+
+
+target_metadata = SQLModel.metadata
+
+
+if context.config.config_file_name is not None:
+    fileConfig(context.config.config_file_name)
+
+
+def run_migrations_online() -> None:
+    connectable = create_engine(app_config.database.url)
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+run_migrations_online()
