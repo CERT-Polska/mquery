60ad22302474ddcb3ce1d88824f51df2dab95b0c
Michał Kolankiewicz <michal.kolankiewicz@nask.pl>
2024-09-27 15:39:55 +0200

    fix: linters

--- src/mqueryfront/vite.config.js
+++ src/mqueryfront/vite.config.js
@@ -1,38 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-const fs = require('fs').promises
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+const fs = require("fs").promises;
 
 export default defineConfig({
-    base: '/',
-    plugins: [react(), svgr({svgrOptions: {}})],
+    base: "/",
+    plugins: [react(), svgr({ svgrOptions: {} })],
     server: {
-        port: 80, 
+        port: 80,
         proxy: {
-            "/api" : { target: "http://dev-web:5000/" },
-            "/docs" : { target: "http://dev-web:5000/" },
-            "/openapi.json" : { target: "http://dev-web:5000/" }
-        }
+            "/api": { target: "http://dev-web:5000/" },
+            "/docs": { target: "http://dev-web:5000/" },
+            "/openapi.json": { target: "http://dev-web:5000/" },
+        },
     },
     esbuild: {
         loader: "jsx",
         include: /src\/.*\.jsx?$/,
-        exclude: []
-    }, 
+        exclude: [],
+    },
     optimizeDeps: {
         esbuildOptions: {
             loader: {
-                '.js': 'jsx'
+                ".js": "jsx",
             },
-            plugins: [{
-                name: "load-js-files-as-jsx",
-                setup(build) { build.onLoad(
-                    { filter: /src\/.*\.js$/ },
-                    async (args) => {
-                        return ({ loader: "jsx", contents: await fs.readFile(args.path, "utf8"), })
-                    });
-                }, },
+            plugins: [
+                {
+                    name: "load-js-files-as-jsx",
+                    setup(build) {
+                        build.onLoad(
+                            { filter: /src\/.*\.js$/ },
+                            async (args) => {
+                                return {
+                                    loader: "jsx",
+                                    contents: await fs.readFile(
+                                        args.path,
+                                        "utf8"
+                                    ),
+                                };
+                            }
+                        );
+                    },
+                },
             ],
         },
     },
-})
+});
