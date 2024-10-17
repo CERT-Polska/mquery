8b8e12c4aee480cd2295bc843e40fd4b8df2123a
mickol34 <80098155+mickol34@users.noreply.github.com>
2024-10-17 13:14:52 +0200

    Hint banners in /status page (#418)

--- src/mqueryfront/src/status/StatusPage.js
+++ src/mqueryfront/src/status/StatusPage.js
@@ -4,6 +4,7 @@ import BackendStatus from "./BackendStatus";
 import DatabaseTopology from "./DatabaseTopology";
 import VersionStatus from "./VersionStatus";
 import api from "../api";
+import WarningPage from "../components/WarningPage";
 
 class StatusPage extends Component {
     constructor(props) {
@@ -26,19 +27,56 @@ class StatusPage extends Component {
             .catch((error) => {
                 this.setState({ error: error });
             });
+        this._ismounted = true;
+    }
+
+    getAgentsUrsaURLDuplicatesWarning(agentgroups) {
+        var ursaURLS = agentgroups.map((agent) => agent.spec.ursadb_url);
+        var duplicateURLS = ursaURLS.filter(
+            (url, index) => ursaURLS.indexOf(url) !== index
+        );
+        if (!duplicateURLS.length) {
+            return null;
+        }
+        return `At least two agents share the same UrsaDB URL(s): \
+        ${duplicateURLS.join(
+            ", "
+        )}. Something might be wrong with backend configuration.`;
+    }
+
+    getNoAgentsWarning(agentgroups) {
+        if (agentgroups.length) {
+            return null;
+        }
+        return "There are no connected agents! Check your backend configuration.";
     }
 
     render() {
+        const ursaURLWarning = this.getAgentsUrsaURLDuplicatesWarning(
+            this.state.backend.agents
+        );
+        const noAgentsWarning = this.getNoAgentsWarning(
+            this.state.backend.agents
+        );
         return (
             <ErrorBoundary error={this.state.error}>
                 <div className="container-fluid">
+                    {this._ismounted && ursaURLWarning && (
+                        <WarningPage msg={ursaURLWarning} dismissable />
+                    )}
                     <h1 className="text-center mq-bottom">Status</h1>
                     <div className="row">
                         <div className="col-md-6">
                             <VersionStatus
                                 components={this.state.backend.components}
                             />
-                            <BackendStatus agents={this.state.backend.agents} />
+                            {this._ismounted && noAgentsWarning ? (
+                                <WarningPage msg={noAgentsWarning} />
+                            ) : (
+                                <BackendStatus
+                                    agents={this.state.backend.agents}
+                                />
+                            )}
                         </div>
                         <div className="col-md-6">
                             <DatabaseTopology />
