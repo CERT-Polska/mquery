368c7338494a698d41dbf44a8ee7843a6b42213a
Michał Kolankiewicz <michal.kolankiewicz@nask.pl>
2025-01-02 13:14:35 +0100

    prettier: styling

--- src/mqueryfront/src/components/ActionShowMatchContext.js
+++ src/mqueryfront/src/components/ActionShowMatchContext.js
@@ -26,33 +26,32 @@ const useClickOutsideModal = (ref, callback) => {
 };
 
 function base64ToHex(str64) {
-    return (
-        atob(str64)
-            .split("")
-            .map(function (aChar) {
-                return ("0" + aChar.charCodeAt(0).toString(16)).slice(-2);
-            })
-            .join("")
-            .toUpperCase()
-    );
+    return atob(str64)
+        .split("")
+        .map(function (aChar) {
+            return ("0" + aChar.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+        .toUpperCase();
 }
 
 function base64ToSanitizedUtf8(str64) {
-    return (
-        atob(str64)
-            .split("")
-            .map(function (aChar) {
-                if (32 <= aChar.charCodeAt(0) && aChar.charCodeAt(0) < 127) {
-                    return aChar;
-                }
-                return ".";
-            })
-            .join("")
-    );
+    return atob(str64)
+        .split("")
+        .map(function (aChar) {
+            if (32 <= aChar.charCodeAt(0) && aChar.charCodeAt(0) < 127) {
+                return aChar;
+            }
+            return ".";
+        })
+        .join("");
 }
 
-function insertEveryNChars (str, insert, n) {
-    return str.split(new RegExp(`(.{${n}})`)).filter(x=>x).join(insert)
+function insertEveryNChars(str, insert, n) {
+    return str
+        .split(new RegExp(`(.{${n}})`))
+        .filter((x) => x)
+        .join(insert);
 }
 
 function cellHTML(foundSample, lineLength, transformFunc) {
@@ -60,14 +59,23 @@ function cellHTML(foundSample, lineLength, transformFunc) {
     const hexMatching = transformFunc(foundSample["matching"]);
     const hexAfter = transformFunc(foundSample["after"]);
     const basicStr = hexBefore + hexMatching + hexAfter;
-    const strWithBreakLines = insertEveryNChars(basicStr, '<br>', lineLength);
+    const strWithBreakLines = insertEveryNChars(basicStr, "<br>", lineLength);
     const breaksInBefore = Math.floor(hexBefore.length / lineLength);
-    const breaksInBeforeAndMatching = Math.floor((hexBefore.length + hexMatching.length) / lineLength);
-    const BoldOpeningTagIndex = hexBefore.length + 4*breaksInBefore;
-    const BoldClosingTagIndex = hexBefore.length + hexMatching.length + 4*breaksInBeforeAndMatching;
-    let boldedStr = strWithBreakLines.slice(0, BoldClosingTagIndex) + '</b>' + strWithBreakLines.slice(BoldClosingTagIndex);
-    boldedStr = boldedStr.slice(0, BoldOpeningTagIndex) + '<b>' + boldedStr.slice(BoldOpeningTagIndex);
-    return boldedStr
+    const breaksInBeforeAndMatching = Math.floor(
+        (hexBefore.length + hexMatching.length) / lineLength
+    );
+    const BoldOpeningTagIndex = hexBefore.length + 4 * breaksInBefore;
+    const BoldClosingTagIndex =
+        hexBefore.length + hexMatching.length + 4 * breaksInBeforeAndMatching;
+    let boldedStr =
+        strWithBreakLines.slice(0, BoldClosingTagIndex) +
+        "</b>" +
+        strWithBreakLines.slice(BoldClosingTagIndex);
+    boldedStr =
+        boldedStr.slice(0, BoldOpeningTagIndex) +
+        "<b>" +
+        boldedStr.slice(BoldOpeningTagIndex);
+    return boldedStr;
 }
 
 const ActionShowMatchContext = (props) => {
@@ -98,17 +106,15 @@ const ActionShowMatchContext = (props) => {
                                 {identifier}
                             </span>
                         </td>
-                        <td
-                            scope="row"
-                            className="text-monospace"
-                        >
-                            {ReactHtmlParser(cellHTML(foundSample, 20, base64ToHex))}
+                        <td scope="row" className="text-monospace">
+                            {ReactHtmlParser(
+                                cellHTML(foundSample, 20, base64ToHex)
+                            )}
                         </td>
-                        <td
-                            scope="row"
-                            className="text-monospace"
-                        >
-                            {ReactHtmlParser(cellHTML(foundSample, 10, base64ToSanitizedUtf8))}
+                        <td scope="row" className="text-monospace">
+                            {ReactHtmlParser(
+                                cellHTML(foundSample, 10, base64ToSanitizedUtf8)
+                            )}
                         </td>
                     </>
                 );
